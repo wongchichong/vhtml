27e2963103a78ee14b75ed7d05e1373403db643b
wongchichong <wongchichong@gmail.com>
2024-04-15 13:20:03 +0800

    \Bump version\

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,19 @@ import { defineConfig } from 'vite'
 /* MAIN */
 
 const config = defineConfig({
-	build: {
-		target: 'esnext',
-		// minify: false,
-		lib: {
-			name: 'vhtml',
-			formats: ['cjs', 'es', 'umd'],
-			entry: "./src/vhtml.ts",
-			fileName: (format: string, entryName: string) => `${entryName}.${format}.js`
-		},
-	},
-	esbuild: {
-		// jsx: 'automatic',
-	},
+    build: {
+        target: 'esnext',
+        // minify: false,
+        lib: {
+            name: 'vhtml',
+            formats: [/*'cjs', '*/'es'/*, 'umd'*/],
+            entry: "./src/vhtml.ts",
+            fileName: (format: string, entryName: string) => `${entryName}.${format}.js`
+        },
+    },
+    esbuild: {
+        // jsx: 'automatic',
+    },
 })
 
 /* EXPORT */
